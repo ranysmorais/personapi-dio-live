ccd9f9f8df477fa46017cb934b091cc4e0bf4831
RanysMorais <ranysmorais@hotmail.com>
2021-09-08 11:44:27 -0300

    criado classes Person, Phone

--- src/main/java/com/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/com/digitalinnovation/personapi/controller/PersonController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
-
     @GetMapping
     public String getBook(){
-        return "API Test: " + "  HELLO WORD";
+
+        return "API Test: " + "  HELLO WORD ";
     }
 }
